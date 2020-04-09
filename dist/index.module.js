f938f699dcd5e59f17bc27462c51dcda8a343c34
TimoBechtel <TimoBechtel@users.noreply.github.com>
2020-04-09 23:45:18 +0000

    deploy: 90e5882ea58e1b9c4f8e93e41b40641c1fcb45dc

---
+++ dist/index.module.js
@@ -0,0 +1,2 @@
+var e,o=function(o,s){void 0===s&&(s=2800),e||(e=document.createElement("div"),document.body.appendChild(e)),e.innerHTML=o,e.className="oh-snack os-show",setTimeout(function(){e.className=e.className.replace("os-show","")},s)};export{o as snack};
+//# sourceMappingURL=index.module.js.map
