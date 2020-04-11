a59f5643290d5c269b3f80a4bc0655dccddc2b88
Timo Bechtel <timo@bechtel.solutions>
2020-04-12 01:18:54 +0200

    feat: add sticky notifications

--- src/index.js
+++ src/index.js
@@ -12,18 +12,54 @@ const createOrGetContainer = () => {
   return snackContainer;
 };
 
-export const snack = (text, duration = 2800) => {
-  let snackElement = document.createElement('div');
-  snackElement.innerHTML = text;
-  snackElement.className = 'oh-snack';
-  createOrGetContainer().appendChild(snackElement);
-  setTimeout(() => {
-    snackElement.className += ' os-show';
-    setTimeout(() => {
-      snackElement.className = snackElement.className.replace('os-show', '');
+const createSnack = (text, parent) => {
+  const element = document.createElement('div');
+  element.className = 'oh-snack';
+  const textElement = document.createElement('p');
+  textElement.innerHTML = text;
+  const content = document.createElement('div');
+  content.appendChild(textElement);
+  content.className = 'os-content';
+  element.appendChild(content);
+  parent.appendChild(element);
+  return {
+    addTopping(htmlElement) {
+      content.appendChild(htmlElement);
+    },
+    show() {
+      element.className += ' os-show';
+    },
+    hide() {
+      element.className = element.className.replace('os-show', '');
       setTimeout(() => {
-        createOrGetContainer().removeChild(snackElement);
+        this.destroy();
       }, 500);
-    }, duration);
+    },
+    destroy() {
+      element.parentElement.removeChild(element);
+    },
+  };
+};
+
+const createButton = (label, action) => {
+  const button = document.createElement('button');
+  button.innerHTML = label;
+  button.addEventListener('click', action);
+  return button;
+};
+
+export const snack = (text, duration) => {
+  const toast = createSnack(text, createOrGetContainer());
+  if (!duration) {
+    const button = createButton('&times;', () => toast.hide());
+    button.className = 'os-close';
+    toast.addTopping(button);
+  }
+  setTimeout(() => {
+    toast.show();
+    if (duration)
+      setTimeout(() => {
+        toast.hide();
+      }, duration);
   }, 10);
 };
